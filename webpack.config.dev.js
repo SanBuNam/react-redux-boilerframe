76b4f21687732a7c21d9509965e36bfb5157f3b0
David Lee <DavidLee8two7@gmail.com>
2019-04-23 23:10:37 -0700

    Webpack Dev server setting

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,5 +13,13 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
     filename: "bundle.js"
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
   }
 };
